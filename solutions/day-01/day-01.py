2fcfd688eb62a7ff7b15fd188a37d832ee8c3f1e
cjharris18 <christopherharris2332@gmail.com>
2021-12-15 23:50:27 +0000

    Add Solution for Day 01.

---
+++ solutions/day-01/day-01.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+def solve_part1(data):
+    increment = 0
+    previous = data[0]
+    for x in data[1:]:
+        if x > previous:
+            increment += 1
+        previous = x
+    return increment
+
+
+def solve_part2(data):
+    increment = 0
+    for y in range(len(data) - 3):
+        if data[y + 3] > data[y]:
+            increment += 1
+    return increment
+
+data = []
+for line in open("day-01-input.txt").readlines():
+    data.extend([int(n) for n in line.split()])
+
+part1 = solve_part1(data)
+print("Part 1: {}".format(part1))
+
+part2 = solve_part2(data)
+print("Part 2: {}".format(part2))
